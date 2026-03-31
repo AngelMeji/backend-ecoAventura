f08a75cdc0c9247885288a5648def69b1a45fecb
AngelMeji <joseangelmejia@gmail.com>
2026-03-31 12:40:32 -0500

    feat: backend password reset link request endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PasswordResetController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/password/email', [PasswordResetController::class, 'sendResetLink']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
